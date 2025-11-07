33023d6adbc704c60326a35d43ef118ed0bb5ccf
saimarala <saiautomation20@gmail.com>
2025-11-07 20:25:29 +0530

    dd

---
+++ TSDEMO/Interface.ts
@@ -0,0 +1,7 @@
+/*
+1.An interface in TypeScript is a way to define the structure of an object
+2.It tells the compiler what properties and types an object should have
+3.It's like a blueprint for objects.
+
+Abstract  method : we only signature of the method (there is no implementation)
+*/
