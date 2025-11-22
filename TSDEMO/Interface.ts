0081c9f448de3f558d16b170d64070f28feab062
saimarala <saiautomation20@gmail.com>
2025-11-22 13:41:56 +0530

    dd

--- TSDEMO/Interface.ts
+++ TSDEMO/Interface.ts
@@ -4,4 +4,27 @@
 3.It's like a blueprint for objects.
 
 Abstract  method : we only signature of the method (there is no implementation)
+
+interface InterfaceName{
+properties 
+abstract methods
+}
+
+1.Regular properties
+2.Optional proparties
+3.Read only properties and function types
+4.Extending interface
+5.Class implements interface
 */
+
+//Example 1: Basic interface
+
+interface Person{
+    name:string;
+    age:number;
+}
+
+let student :Person{
+    name:"John",
+    age:30
+}
